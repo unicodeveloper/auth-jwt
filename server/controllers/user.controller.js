64c301ea3b0f7a0e62ff8e0f5d742d0e58d3b846
busayo <prosperotemuyiwa@gmail.com>
2015-06-05 16:06:42 +0100

    Verify uniqueness of username and email

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -7,19 +7,20 @@ module.exports = {
   },
 
   registerUser: function(req, res){
+    console.log( req.body );
     var user  = new User();
     var token = jwt.sign(user, secrets.sessionSecret, { expiresInMinutes: 1440 });
 
-    user.username = req.body.username;
+    user.username  = req.body.username;
     user.email     = req.body.email;
     user.password  = user.hashPassword(req.body.password);
 
     user.save( function(err, users){
       if(err) {
-        if(err.name == 'MongoError') {
-          res.status(400).json({ err: err, error: 'Email is already registered before. Please choose another' });
-        } else {
-          res.status(500).json({ error: 'Server error' });
+        if(err.name == 'MongoError' && err.message.indexOf('$email_1') > 0 ) {
+          res.status(400).json({ error: 'Email is already registered. Please choose another' });
+        }else if ( err.name == 'MongoError' && err.message.indexOf('$username_1') > 0) {
+          res.status(500).json({ error: 'Username is already taken. Please choose another' });
         }
       } else {
         res.status(200).json({ token: token, success: true, message: "User Registered successfully" });
